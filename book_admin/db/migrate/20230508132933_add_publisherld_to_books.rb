60a6dcc37f77c77d24732020acfcce38aa466694
EC2 Default User <itakaha71@gmail.com>
2023-05-08 15:31:46 +0000

    [Finish]relation

--- book_admin/db/migrate/20230508132933_add_publisherld_to_books.rb
+++ book_admin/db/migrate/20230508132933_add_publisherld_to_books.rb
@@ -1,5 +1,6 @@
 class AddPublisherldToBooks < ActiveRecord::Migration[6.1]
   def change
     add_reference :books, :publisher, null: false, foreign_key: true
+    change_column :books, :publisher_id, :integer, null: false
   end
 end

c4a2d60649fae8d812145639a59b14616b20b038
EC2 Default User <itakaha71@gmail.com>
2023-05-10 04:22:28 +0000

    [Finish]2-2-3

--- book_admin/app/models/book.rb
+++ book_admin/app/models/book.rb
@@ -1,6 +1,17 @@
 class Book < ApplicationRecord
   scope :costly, -> { where("price > ?", 3000)}
   scope :written_about, ->(theme){where("name like ?", "%#{theme}%")}
-  
+
   belongs_to :publisher
+
+  has_many :authors, through: :book_authors
+
+  validates :name, presence: true
+  validates :name, length: { maximum: 25 }
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+  validate do |book|
+    if book.name.include?("exercise")
+      book.errors[:name] << "I don't like execise."
+    end
+  end
 end

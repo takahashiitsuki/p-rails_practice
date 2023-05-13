bf1bd840261df44c5fb63c512112d71d7949d268
EC2 Default User <itakaha71@gmail.com>
2023-05-13 05:00:47 +0000

    [Finish]2-2-4

--- book_admin/app/models/book.rb
+++ book_admin/app/models/book.rb
@@ -14,4 +14,17 @@ class Book < ApplicationRecord
       book.errors[:name] << "I don't like execise."
     end
   end
+  
+  after_destroy do
+    Rails.logger.info "Book is deleted: #{self.attributes}"
+  end
+  
+  after_destroy :if => :high_price? do
+    Rails.logger.warn "Book with high price is deleted: #{self.attributes}"
+    Rails.logger.warn "Please check!!"
+  end
+  
+  def high_price?
+    price >= 5000
+  end
 end

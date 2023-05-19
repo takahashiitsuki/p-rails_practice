d5daef7217115c15b594eb7d8c030b7e676df75c
EC2 Default User <itakaha71@gmail.com>
2023-05-19 21:05:11 +0000

    [Finish]2-3-2

--- book_admin/app/controllers/books_controller.rb
+++ book_admin/app/controllers/books_controller.rb
@@ -1,9 +1,20 @@
 class BooksController < ApplicationController
+  protect_form_forgery except: [:destroy] #
+  
   def show
     @book = Book.find(params[:id]) #
     respond_to do |format| #
       format.html
       format.json
     end
+  end
+  
+  # destroy メソッドを追加
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    respond_to_do |format|
+      format.html { redirect_to "/" }
+      format.json { head :no_content }
   end
 end

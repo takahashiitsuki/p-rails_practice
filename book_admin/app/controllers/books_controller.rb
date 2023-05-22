b272bc73df56b41c828295f39770366f3bb8d206
EC2 Default User <itakaha71@gmail.com>
2023-05-22 14:41:14 +0000

    [Fix]2-3-2

--- book_admin/app/controllers/books_controller.rb
+++ book_admin/app/controllers/books_controller.rb
@@ -1,5 +1,7 @@
 class BooksController < ApplicationController
   protect_form_forgery except: [:destroy] #
+  before_action :set_book, only: [:show, :destroy]
+  around_action :action_logger, only: [:destroy]
   
   def show
     @book = Book.find(params[:id]) #
@@ -16,5 +18,17 @@ class BooksController < ApplicationController
     respond_to_do |format|
       format.html { redirect_to "/" }
       format.json { head :no_content }
+  end
+  
+  private
+  
+  def set_book
+    @book = Book.find(params[:id])
+  end
+  
+  def action_logger
+    logger.info "around-before"
+    yield
+    logger.info "around-after"
   end
 end

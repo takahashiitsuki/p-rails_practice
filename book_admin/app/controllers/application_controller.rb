6a97f8604b006aab0972040be01c57686bbe9176
EC2 Default User <itakaha71@gmail.com>
2023-05-27 05:14:58 +0000

    [Finish]2-3-4

--- book_admin/app/controllers/application_controller.rb
+++ book_admin/app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
+rescue_from LoginFailed, with: :login_failed
+  
+  def login_failed
+    rendetr template: "shared/login_failed", status: 401
+  end
+  
   before_action do
     redirect_to access_denied_path if params[:token].blank?
   end

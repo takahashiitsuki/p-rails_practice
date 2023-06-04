bc1dc3d472c86f49c8b2f11d447c900048669a72
EC2 Default User <itakaha71@gmail.com>
2023-06-04 07:09:47 +0000

    [Finish]2-4-2

--- book_admin/app/controllers/application_controller.rb
+++ book_admin/app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
 rescue_from LoginFailed, with: :login_failed
+before_action :detect_mobile_variant
   
   def login_failed
     rendetr template: "shared/login_failed", status: 401
@@ -7,5 +8,10 @@ rescue_from LoginFailed, with: :login_failed
   
   before_action do
     redirect_to access_denied_path if params[:token].blank?
+  end
+  
+  private
+  def detect_mobile_variant
+    request.variant = :mobile if request.user_agent =~ /iPhone/
   end
 end

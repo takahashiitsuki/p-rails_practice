bb9b7bb6d113f50d1734649831ad758f3b2a67e0
EC2 Default User <itakaha71@gmail.com>
2023-06-09 13:04:57 +0000

    [Start]3

---
+++ rails_test_sample/db/migrate/20230609130400_create_todos.rb
@@ -0,0 +1,11 @@
+class CreateTodos < ActiveRecord::Migration[6.1]
+  def change
+    create_table :todos do |t|
+      t.string :title
+      t.string :string
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end

dba10023b0af784464e84bd9d82a87f27ef569a8
EC2 Default User <itakaha71@gmail.com>
2023-06-21 03:29:31 +0000

    [Finish]3-1-8

--- rails_test_sample/test/models/todo_test.rb
+++ rails_test_sample/test/models/todo_test.rb
@@ -4,4 +4,24 @@ class TodoTest < ActiveSupport::TestCase
   test "the truth" do
     assert false
   end
+  
+  test "sleep 1" do
+    sleep 3
+    assert true
+  end
+  
+  test "sleep 2" do
+    sleep 3
+    asset true
+  end
+  
+  test "sleep 3" do
+    sleep 3
+    asset true
+  end
+  
+  test "sleep 4" do
+    sleep 3
+    asset true
+  end
 end

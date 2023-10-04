e84129c44a5e169115e3c9eb79c58533911e8bb5
EC2 Default User <itakaha71@gmail.com>
2023-10-04 02:54:22 +0000

    [Fix]text4-1-5

--- webpacker_sample/config/environments/development.rb
+++ webpacker_sample/config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  config.hosts << "4b0506e3bf9f4d84a2366f8dcb5b8fea.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
